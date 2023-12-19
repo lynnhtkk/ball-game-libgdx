51a7f620f6a9a3c2a93583c42298acf8be7e95fc
Nyi Nyi Linn Htet <nyi20471@gmail.com>
2023-12-19 23:52:45 +0100

    Add block class

---
+++ core/src/com/mygdx/game/Block.java
@@ -0,0 +1,21 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class Block {
+    int x;
+    int y;
+    int width;
+    int height;
+
+    public Block(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void draw(ShapeRenderer shape) {
+        shape.rect(x, y, width, height);
+    }
+}
